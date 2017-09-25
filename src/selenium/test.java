11bc86b75c1b61962881fb129e8a6b0240fdb09e
absin1 <abhinav1@istarindia.com>
2017-09-25 13:34:04 +0530

    first

---
+++ src/selenium/test.java
@@ -0,0 +1,14 @@
+import junit.framework.TestSuite;
+
+public class Test {
+
+  public static Test suite() {
+    TestSuite suite = new TestSuite();
+    suite.addTestSuite(gmail.class);
+    return suite;
+  }
+
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(suite());
+  }
+}
