a31248509c9d925e5eeff623e95a5c177c24020c
absin1 <abhinav1@istarindia.com>
2017-09-25 17:26:17 +0530

    phantom driver added

--- src/seleniumClasses/Phantom.java
+++ src/seleniumClasses/Phantom.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-public class phantom {
+public class Phantom {
 
 	public static void main(String[] args) throws IOException {
 
